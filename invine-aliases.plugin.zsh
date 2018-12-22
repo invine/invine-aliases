7230747ba299c6642938bb48e3c8acd326d6bad5
invine <invine.invine@yandex.ru>
2018-12-23 02:35:21 +0300

    initial version

---
+++ invine-aliases.plugin.zsh
@@ -0,0 +1,22 @@
+export LESSOPEN="| /usr/bin/source-highlight-esc.sh %s"
+export LESS='-R '
+export GREP_COLORS="mt=01;33"
+export EDITOR="vim"
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias mkdir='mkdir -p -v'
+alias nn='nano -w'
+alias snn='sudo nano -w'
+alias svim='sudo vim'
+alias ping='ping -c 5'
+alias dmesg='dmesg -HL'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -I'
+
+source /usr/share/doc/pkgfile/command-not-found.zsh
+
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey "${terminfo[kend]}" end-of-line
+bindkey "${terminfo[kdch1]}" delete-char
