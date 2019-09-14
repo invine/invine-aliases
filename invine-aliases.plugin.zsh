f4ee134d115f79eb899ec98602ecc018e68b5094
Pavel Frolov <artyhere@gmail.com>
2019-09-15 01:09:14 +0300

    add keybindings for jj for zsh and for history-substring-search

--- invine-aliases.plugin.zsh
+++ invine-aliases.plugin.zsh
@@ -27,8 +27,29 @@ alias vimrc='${=EDITOR} ~/.config/nvim/init.vim'
 alias vim=nvim
 alias help=tldr
 
-# source /usr/share/doc/pkgfile/command-not-found.zsh
-
+# fix home, delete and return bindings
 bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
 bindkey "${terminfo[kdch1]}" delete-char
+
+# bind jj to ESC
+bindkey "jj" vi-cmd-mode
+
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
